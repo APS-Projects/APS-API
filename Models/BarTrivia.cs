ac3016d38e286bac7f538dc77c306f6b37a65df1
awieckert <wiec1369@gmail.com>
2019-01-06 16:56:00 -0600

    Added the data models. Successful migration and update of database

---
+++ Models/BarTrivia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APS_API.Models
+{
+    public class BarTrivia
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int BarId { get; set; }
+
+        [Required]
+        public int DayOfWeek { get; set; }
+
+        [Required]
+        [Range (0, 2400)]
+        public int Hours { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public Bar Bar { get; set; }
+    }
+}
