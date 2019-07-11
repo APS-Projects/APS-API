20371cde78bbfb2b0ffa1b5958e09b8485653171
awieckert <wiec1369@gmail.com>
2019-07-10 20:34:00 -0500

    Have the data in it's final form. Need to create reference type for dapper to merge multiple items

--- Tools/TriviaDrunksScraper/HappyHours/HappyHourHtmlParsers.cs
+++ Tools/TriviaDrunksScraper/HappyHours/HappyHourHtmlParsers.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TriviaDrunksScraper.HappyHours
@@ -30,11 +31,64 @@ namespace TriviaDrunksScraper.HappyHours
                     .Where(node => node.HasClass("basic-container"))
                     .Select(node => node.Descendants("li")).SingleOrDefault();
 
-                var barNames = happyHours.Select(node => node.Descendants("a").SingleOrDefault().InnerHtml);
+                var parsedBarNames = ParseBarNames(happyHours);
 
+                var happyHourCopy = new List<HtmlNode>(happyHours);
 
+                var parsedDescriptions = ParseHappyHourDescription(happyHourCopy);
+
+                //Build a list of reference types with correct properties and pass it to dapper like below
+                //string processQuery = "INSERT INTO PROCESS_LOGS VALUES (@A, @B)";
+                //connection.Execute(processQuery, processList);
 
                 return happyHours;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private IEnumerable<string> ParseBarNames(IEnumerable<HtmlNode> happyHours)
+        {
+            try
+            {
+                var barNames = happyHours.Select(node => node.Descendants("a").SingleOrDefault().InnerHtml);
+
+                var parsedBarNames = new List<string>();
+
+                foreach (var item in barNames)
+                {
+                    var replaceApostrophe = Regex.Replace(item, @"&#8217;", "'").Trim();
+                    var replaceAmp = Regex.Replace(replaceApostrophe, @"&#038;", "&");
+                    var replaceDash = Regex.Replace(replaceAmp, @"&#8211;", "-");
+                    parsedBarNames.Add(replaceDash);
+                }
+
+                return parsedBarNames;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private IEnumerable<string> ParseHappyHourDescription(IEnumerable<HtmlNode> happyHours)
+        {
+            try
+            {
+                var parsedDescriptions = new List<string>();
+
+                foreach (var item in happyHours)
+                {
+                    item.RemoveChild(item.SelectSingleNode("a"));
+
+                    var itemText = item.InnerText.Trim().Remove(0, 1); //removes initial semi-colon
+                    var replaceLineBreaks = Regex.Replace(itemText, @"\t|\n|\r|", "").Trim();
+                    parsedDescriptions.Add(replaceLineBreaks);
+                }
+
+                return parsedDescriptions;
             }
             catch (Exception ex)
             {
